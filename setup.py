7d1829c7b373d964424e59c72dfb271defed430b
Raphael Cousin <racousin@student.42.fr>
2025-10-11 20:46:32 +0200

    Bump permuted_mnist version to 0.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="permuted_mnist",
-    version="0.14",
+    version="0.15",
     packages=find_packages(),
     include_package_data=True,  # Important for including data files
     package_data={
